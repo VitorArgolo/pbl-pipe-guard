9b688c06a1a3c6b7fd8cb95965e9dfa0457016c4
VitorArgolo <83723061+VitorArgolo@users.noreply.github.com>
2024-05-22 14:03:32 -0300

    build

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://api-login-pptt.onrender.com';
   private authState = new BehaviorSubject<boolean>(this.isAuthenticated());
   authState$ = this.authState.asObservable();
 
