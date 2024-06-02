a41e0735f3bb8a130abf171bef61d15a76722733
VitorArgolo <83723061+VitorArgolo@users.noreply.github.com>
2024-06-02 00:43:10 -0300

    style

--- src/app/monitoring-list/monitoring-list.component.ts
+++ src/app/monitoring-list/monitoring-list.component.ts
@@ -8,9 +8,9 @@ import { MonitoringService } from '../monitoring.service';
   styleUrls: ['./monitoring-list.component.css']
 })
 export class MonitoringListComponent implements OnInit {
-  logs: any[] = []; 
+  logs: any[] = [];
 
-  constructor(private monitoringService: MonitoringService) {}
+  constructor(private monitoringService: MonitoringService) { }
 
   ngOnInit(): void {
     this.loadLogs();
@@ -26,4 +26,25 @@ export class MonitoringListComponent implements OnInit {
       }
     );
   }
+  getLogClass(method: string): string {
+    
+    switch (method) {
+      case 'GET':
+        return 'get-log';
+      case 'POST':
+        return 'post-log';
+      case 'PUT':
+        return 'put-log';
+      case 'DELETE':
+        return 'delete-log';
+      default:
+        return '';
+    }
+  }
+  getHttpMethod(message: string): string {
+    const regex = /(GET|POST|PUT|DELETE)/i; // Expressão regular para encontrar o método HTTP
+    const match = message.match(regex);
+    return match ? match[0].toUpperCase() : ''; // Retorna o método encontrado em maiúsculas ou uma string vazia se não for encontrado
+  }
+  
 }
